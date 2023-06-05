4b211a5b80d70c7b053eb44b3c149cc1a994d639
Maryia Mazurava <108463389+maryia-mazurava@users.noreply.github.com>
2023-06-05 17:30:23 +0200

    Update basic.py

--- src/calculator/calclib/basic.py
+++ src/calculator/calclib/basic.py
@@ -1,63 +1,64 @@
 """!
-@package calculator
-@file basic.py
-@author Alina Vinogradova
-Implementation of basic mathematical operations for the calculator
+    @file basic.py
+
+    @brief Implementation of the basic mathematical operations library for the calculator.
+
+    @author Alina Vinogradova
+
+    @date 26.04.2023
+
 """
 
-"""!
-Base class "Basic"
-Representation of basic mathematical operations
-"""
+
 class Basic:
     """!
-    A class variables:
-    pi - constant
-    exp - constant
+        @brief Base class "Basic", representation of basic mathematical operations
     """
+
     pi = 3.1416
     exp = 2.7183
 
-    """!
-    The constructor
-    """
     def __init__(self):
         pass
 
-    """!
-    Method for + operation
-    @param x First operand
-    @param y Second operand
-    @return Sum of two numbers
-    """
     def add(self, x: float, y: float):
+        """!
+            @brief Method for + operation
+            @param x First operand
+            @param y Second operand
+            @return Sum of two numbers
+        """
+
         return self.int_translate(x + y)
 
-    """!
-    Method for - operation
-    @param minuend First operand
-    @param subtrahend Second operand
-    @return Difference between two numbers
-    """
     def sub(self, minuend: float, subtrahend: float):
+        """!
+            @brief Method for - operation
+            @param minuend First operand
+            @param subtrahend Second operand
+            @return Difference between two numbers
+        """
+
         return self.int_translate(minuend - subtrahend)
 
-    """!
-    Method for * operation
-    @param multiplier First operand
-    @param multiplicand Second operand
-    @return Product of two numbers
-    """
     def mul(self, multiplier: float, multiplicant: float):
+        """!
+            @brief Method for * operation
+            @param multiplier First operand
+            @param multiplicant Second operand
+            @return Product of two numbers
+        """
+
         return self.int_translate(multiplier * multiplicant)
 
-    """!
-    Method for / operation
-    @param dividend The number to be divided
-    @param divisor The number to divide by
-    @return Division operation result
-    """
     def div(self, divident: float, divisor: float):
+        """!
+            @brief Method for / operation
+            @param divident The number to be divided
+            @param divisor The number to divide by
+            @return Division operation result
+        """
+
         if divisor == 0:
             raise ZeroDivisionError
         else:
@@ -66,6 +67,12 @@ class Basic:
 
     @staticmethod
     def int_translate(num):
+        """!
+            @brief Method for making an integer number of float
+            @param num The number to be rounded
+            @return Rounded number
+        """
+
         if isinstance(num, complex):
             num = num.real
         if float(num).is_integer():
